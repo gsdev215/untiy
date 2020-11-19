46a6e7edd859a333d2f0c219a935fb1ab539d3f2
bartick <69100224+bartick@users.noreply.github.com>
2020-11-19 15:01:21 +0530

    Added Cogs

--- mmbot.py
+++ mmbot.py
@@ -1,50 +1,38 @@
 import discord
+from dotenv import load_dotenv
 from itertools import cycle
 from discord.ext import commands, tasks
 import random
 import os
+
+load_dotenv()
+
 bot = commands.Bot(command_prefix=('.'))
+bot.remove_command('help')
 
 status = cycle(['My Prefix is .', 'Originally I was developed by Dev','My developers username with tag is gs.dev#4082', 'Don\'t try to mess withme and my code','DM me for help | no! don`t dm '])
 
 @bot.event 
 async def on_ready():
 	change_status.start()
-	print('online')
+	print(f'Logged in as {bot.user.name}')
 	
 	
-@tasks.loop(seconds=5)
+@tasks.loop(seconds=20)
 async def change_status():
     await bot.change_presence(activity = discord.Game(next(status)))
-
-@bot.command()
-async def add(ctx, left: int, right: int):
-    """Adds two numbers together."""
-    await ctx.send(left + right)
     
 @bot.command()
 async def Ping(ctx):
-    """Adds two numbers together."""
-    await ctx.send(Pong)
-    
-@bot.command()
-async def sub(ctx, left: int, right: int):
-    """Adds two numbers together."""
-    await ctx.send(left - right)
-    
-@bot.command()
-async def mul(ctx, left: int, right: int):
-    """Adds two numbers together."""
-    await ctx.send(left * right)
-    
-@bot.command()
-async def power(ctx, left: int, right: int):
-    """Adds two numbers together."""
-    await ctx.send((left) ^ right)
-    
-@bot.command()
-async def div(ctx, left: int, right: int):
-    """Adds two numbers together."""
-    await ctx.send(left / right)
+    before = time.monotonic()
+    before_ws = int(round(self.bot.latency * 1000, 1))
+    message = await ctx.send("🏓 Pong")
+    ping = (time.monotonic() - before) * 1000
+    await message.edit(content=f"🏓 WS: {before_ws}ms  |  REST: {int(ping)}ms")
 
-bot.run(os.environ('TOKEN')
+for file in os.listdir("cogs"):
+    if file.endswith(".py"):
+        name = file[:-3]
+        bot.load_extension(f"cogs.{name}")
+
+bot.run(os.getenv('TOKEN'))
