5351fbb58f810d10396c403f1b2f982d3ab46189
bartick <69100224+bartick@users.noreply.github.com>
2020-12-16 18:49:27 +0530

    Added 2 intent

--- mmbot.py
+++ mmbot.py
@@ -8,7 +8,9 @@ import os
 
 load_dotenv()
 
-bot = commands.Bot(command_prefix=('.'))
+intents = discord.Intents.all()
+
+bot = commands.Bot(command_prefix=('.'), intents=intents)
 bot.remove_command('help')
 
 status = cycle(['My Prefix is .', 'Originally I was developed by Dev','My developers username with tag is gs.dev#4082', 'Don\'t try to mess withme and my code','DM me for help | no! don`t dm '])
@@ -37,6 +39,24 @@ async def addrole(ctx):
     user = ctx.message.author
     role = discord.utils.get(user.server.roles, name="Test")
     await client.add_roles(user, role)
+
+@bot.event
+async def on_member_join(member):
+    channel = bot.get_channel(768690733673611284)
+    embed = discord.Embed(title="Welcome :-", description="Thank you for kindly joining our server", color=0x00ff00)
+    embed.set_author(name=member.name, icon_url=member.avatar_url)
+    embed.add_field(name="IP" , value="play.unitypixel.net", inline=True) 
+    embed.add_field(name="Port", value="19132", inline=True) 
+    embed.set_footer(text="Hope u stay here longer")
+    await channel.send(content=f"{member.mention}",embed=embed)
+
+@bot.event
+async def on_member_remove(member):
+    channel = bot.get_channel(731906813720789022)
+    embed = discord.Embed(title="Good Bye :-", description="Thank you for being with us hope to see you soon.", color=0x00ff00)
+    embed.set_author(name=member.name, icon_url=member.avatar_url)
+    await member.send(embed=embed)
+    await channel.send(embed=embed)
 
 for file in os.listdir("cogs"):
     if file.endswith(".py"):
