5272dbffdc6536477581b2bf37fc36c8cbb9b2d2
gsdev215 <74264993+gsdev215@users.noreply.github.com>
2020-12-17 14:57:55 +0530

    Update embed.py

--- cogs/embed.py
+++ cogs/embed.py
@@ -16,3 +16,8 @@ bot.remove_command('help')
 @bot.commands
 async def say (ctx, arg):
     await ctx.send(arg)
+
+@bot.command()
+async def slap(ctx, members: commands.Greedy[discord.Member], *, reason='no reason'):
+    slapped = ", ".join(x.name for x in members)
+    await ctx.send('{} just got slapped for {}'.format(slapped, reason))
