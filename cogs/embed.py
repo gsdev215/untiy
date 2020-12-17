0d36e20e3df50b2cf52f59cf8cf694a9f5343e26
gsdev215 <74264993+gsdev215@users.noreply.github.com>
2020-12-17 14:44:21 +0530

    Update embed.py

--- cogs/embed.py
+++ cogs/embed.py
@@ -12,3 +12,7 @@ intents = discord.Intents.all()
 
 bot = commands.Bot(command_prefix=('.'), intents=intents)
 bot.remove_command('help')
+
+@bot.commands
+async def say (ctx, arg):
+    await ctx.send(arg)
