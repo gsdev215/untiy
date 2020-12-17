5e3e76364e3fdd7bd2703ff3ffa727a01432ba73
gsdev215 <74264993+gsdev215@users.noreply.github.com>
2020-12-17 15:01:00 +0530

    Update embed.py

--- cogs/embed.py
+++ cogs/embed.py
@@ -1,23 +1,22 @@
 import discord
 from dotenv import load_dotenv
 from itertools import cycle
-from discord.ext import commands, tasks
+from discord.ext import commands
 import random
 import time
 import os
+from mmbot import bot
 
-load_dotenv()
 
-intents = discord.Intents.all()
+class Fun(commands.Cog):
+	def __init__(self,bot):
+		self.bot = bot
 
-bot = commands.Bot(command_prefix=('.'), intents=intents)
-bot.remove_command('help')
-
-@bot.commands
+@commands.command()
 async def say (ctx, arg):
     await ctx.send(arg)
 
-@bot.command()
+@commands.command()
 async def slap(ctx, members: commands.Greedy[discord.Member], *, reason='no reason'):
     slapped = ", ".join(x.name for x in members)
     await ctx.send('{} just got slapped for {}'.format(slapped, reason))
