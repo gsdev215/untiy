a63860ceb25c516b28eb0a03ec60c8a5a73f657c
gsdev215 <74264993+gsdev215@users.noreply.github.com>
2021-01-06 22:31:45 +0530

    Update mmbot.py

--- mmbot.py
+++ mmbot.py
@@ -29,20 +29,20 @@ async def change_status():
     
 
 @bot.command()
-async def load(extension_name : str):
+async def load(ctx , extension_name : str):
     """Loads an extension."""
     try:
         bot.load_extension(extension_name)
     except (AttributeError, ImportError) as e:
-        await bot.say("```py\n{}: {}\n```".format(type(e).__name__, str(e)))
+        await ctx.send("```py\n{}: {}\n```".format(type(e).__name__, str(e)))
         return
-    await bot.say("{} loaded.".format(extension_name))
+    await ctx.send("{} loaded.".format(extension_name))
 
 @bot.command()
-async def unload(extension_name : str):
+async def unload(ctx, extension_name : str):
     """Unloads an extension."""
     bot.unload_extension(extension_name)
-    await bot.say("{} unloaded.".format(extension_name))
+    await ctx.send("{} unloaded.".format(extension_name))
 
 @bot.command()
 async def ping(ctx):
