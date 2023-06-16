8f592f9fdcb2b759cea97c709b1295f29650a274
Dev Bhattacharjee <74264993+gsdev215@users.noreply.github.com>
2023-06-16 10:13:23 +0500

    Update and rename mmbot.py to bot.py

--- bot.py
+++ bot.py
@@ -28,25 +28,6 @@ async def change_status():
     await bot.change_presence(activity = discord.Game(next(status)))
     
 
-'''@bot.command()
-async def load(ctx , extension_name : str):
-    """Loads an extension."""
-    try:
-        bot.load_extension(extension_name)
-    except (AttributeError, ImportError) as e:
-        await ctx.send("```py\n{}: {}\n```".format(type(e).__name__, str(e)))
-        return
-    await ctx.send("{} loaded.".format(extension_name))
-
-@bot.command()
-async def unload(ctx, extension_name : str):
-    """Unloads an extension."""
-    bot.unload_extension(extension_name)
-    await ctx.send("{} unloaded.".format(extension_name))'''
-'''@bot.commands ()
- async def reload(extension):
-       bot.reload_extension('more')'''
-
 @bot.command()
 async def ping(ctx):
     before = time.monotonic()
